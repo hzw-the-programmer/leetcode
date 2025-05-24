74ccec5f80f0b0233225cf81f1d4b0ff315d139a
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-25 06:32:35 +0800

    s144 take instead of clone

--- src/s144/iterative.rs
+++ src/s144/iterative.rs
@@ -6,11 +6,26 @@ pub fn preorder_traversal(root: Tree) -> Vec<i32> {
     stack.push(root);
     while let Some(mut tree) = stack.pop() {
         while let Some(node) = tree {
-            let node = node.borrow();
+            let mut node = node.borrow_mut();
             res.push(node.val);
-            stack.push(node.right.clone());
-            tree = node.left.clone();
+            stack.push(node.right.take());
+            tree = node.left.take();
         }
     }
     res
 }
+
+// pub fn preorder_traversal(root: Tree) -> Vec<i32> {
+//     let mut res = vec![];
+//     let mut stack = vec![];
+//     stack.push(root);
+//     while let Some(mut tree) = stack.pop() {
+//         while let Some(node) = tree {
+//             let node = node.borrow();
+//             res.push(node.val);
+//             stack.push(node.right.clone());
+//             tree = node.left.clone();
+//         }
+//     }
+//     res
+// }
