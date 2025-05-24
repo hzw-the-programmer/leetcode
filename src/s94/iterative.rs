a8d42a3e610ef21c22fb0f59faa1bb2fa8f9e2d5
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-24 18:57:59 +0800

    tree concept

--- src/s94/iterative.rs
+++ src/s94/iterative.rs
@@ -4,16 +4,16 @@ pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     let mut res = Vec::new();
 
     let mut stack = vec![];
-    let mut node = root;
-    while !node.is_none() || !stack.is_empty() {
-        while let Some(rc) = node {
-            stack.push(rc.clone());
-            node = rc.borrow().left.clone();
+    let mut tree = root;
+    while tree.is_some() || !stack.is_empty() {
+        while let Some(node) = tree {
+            stack.push(node.clone());
+            tree = node.borrow().left.clone();
         }
 
-        if let Some(rc) = stack.pop() {
-            res.push(rc.borrow().val);
-            node = rc.borrow().right.clone();
+        if let Some(node) = stack.pop() {
+            res.push(node.borrow().val);
+            tree = node.borrow().right.clone();
         }
     }
 
