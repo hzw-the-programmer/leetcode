3ba20b771afd26403769f37ba2f82cef67a5324c
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-24 18:59:58 +0800

    Tree

--- src/s94/recursive.rs
+++ src/s94/recursive.rs
@@ -6,7 +6,7 @@ pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     res
 }
 
-fn inorder_traversal_recursive(root: &Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+fn inorder_traversal_recursive(root: &Tree, res: &mut Vec<i32>) {
     if let Some(node) = root {
         let node = node.borrow();
         inorder_traversal_recursive(&node.left, res);
