2c7d8d83f1de1aa62854cb5c5876449ff25c1397
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-24 19:07:37 +0800

    Tree concept

--- src/s98/tests.rs
+++ src/s98/tests.rs
@@ -2,58 +2,26 @@ use super::*;
 
 #[test]
 fn test_is_valid_bst() {
-    let root = Some(Rc::new(RefCell::new(TreeNode {
-        val: 2,
-        left: Some(Rc::new(RefCell::new(TreeNode {
-            val: 1,
-            left: None,
-            right: None,
-        }))),
-        right: Some(Rc::new(RefCell::new(TreeNode {
-            val: 3,
-            left: None,
-            right: None,
-        }))),
-    })));
+    let root = new_tree(2, new_tree(1, None, None), new_tree(3, None, None));
     assert_eq!(is_valid_bst(root), true);
 }
 
 #[test]
 fn test_is_valid_bst_2() {
-    let root = Some(Rc::new(RefCell::new(TreeNode {
-        val: 5,
-        left: Some(Rc::new(RefCell::new(TreeNode {
-            val: 1,
-            left: None,
-            right: None,
-        }))),
-        right: Some(Rc::new(RefCell::new(TreeNode {
-            val: 4,
-            left: Some(Rc::new(RefCell::new(TreeNode {
-                val: 3,
-                left: None,
-                right: None,
-            }))),
-            right: Some(Rc::new(RefCell::new(TreeNode {
-                val: 6,
-                left: None,
-                right: None,
-            }))),
-        }))),
-    })));
+    let root = new_tree(
+        5,
+        new_tree(1, None, None),
+        new_tree(4, new_tree(3, None, None), new_tree(6, None, None)),
+    );
     assert_eq!(is_valid_bst(root), false);
 }
 
 #[test]
 fn test_is_valid_bst_3() {
-    let root = Some(Rc::new(RefCell::new(TreeNode::new(
+    let root = new_tree(
         5,
-        Some(Rc::new(RefCell::new(TreeNode::new(4, None, None)))),
-        Some(Rc::new(RefCell::new(TreeNode::new(
-            6,
-            Some(Rc::new(RefCell::new(TreeNode::new(3, None, None)))),
-            Some(Rc::new(RefCell::new(TreeNode::new(7, None, None)))),
-        )))),
-    ))));
+        new_tree(4, None, None),
+        new_tree(6, new_tree(3, None, None), new_tree(7, None, None)),
+    );
     assert_eq!(is_valid_bst(root), false);
 }
