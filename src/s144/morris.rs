9b93865f936b24bc0369a638c19a5437823e5182
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-25 16:24:45 +0800

    preorder traversal morris

--- src/s144/morris.rs
+++ src/s144/morris.rs
@@ -4,7 +4,11 @@ pub fn preorder_traversal(mut root: Tree) -> Vec<i32> {
     let mut res = vec![];
 
     while let Some(node) = root {
-        if let Some(mut predecessor) = node.borrow().left.clone() {
+        if node.borrow().left.is_none() {
+            res.push(node.borrow().val);
+            root = node.borrow().right.clone();
+        } else {
+            let mut predecessor = node.borrow().left.clone().unwrap();
             while predecessor.borrow().right.is_some() {
                 let right_node = predecessor.borrow().right.clone().unwrap();
                 if std::rc::Rc::ptr_eq(&right_node, &node) {
@@ -12,7 +16,6 @@ pub fn preorder_traversal(mut root: Tree) -> Vec<i32> {
                 }
                 predecessor = right_node;
             }
-
             if predecessor.borrow().right.is_none() {
                 res.push(node.borrow().val);
                 root = node.borrow().left.clone();
@@ -21,9 +24,6 @@ pub fn preorder_traversal(mut root: Tree) -> Vec<i32> {
                 predecessor.borrow_mut().right = None;
                 root = node.borrow().right.clone();
             }
-        } else {
-            res.push(node.borrow().val);
-            root = node.borrow().right.clone();
         }
     }
 
