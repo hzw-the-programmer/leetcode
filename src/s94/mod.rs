e892296d3a3b67f761fec4bb65228fe0e330fadd
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-25 17:01:28 +0800

    inorder traversal morris

--- src/s94/mod.rs
+++ src/s94/mod.rs
@@ -3,8 +3,11 @@
 // mod recursive;
 // pub use recursive::inorder_traversal;
 
-mod iterative;
-pub use iterative::inorder_traversal;
+// mod iterative;
+// pub use iterative::inorder_traversal;
+
+mod morris;
+pub use morris::inorder_traversal;
 
 #[cfg(test)]
 mod tests;
---
+++ src/s94/morris.rs
@@ -0,0 +1,31 @@
+use crate::binary_tree::*;
+
+pub fn inorder_traversal(mut root: Tree) -> Vec<i32> {
+    let mut res = vec![];
+
+    while let Some(node) = root {
+        if node.borrow().left.is_none() {
+            res.push(node.borrow().val);
+            root = node.borrow().right.clone();
+        } else {
+            let mut predecessor = node.borrow().left.clone().unwrap();
+            while predecessor.borrow().right.is_some() {
+                let right_node = predecessor.borrow().right.clone().unwrap();
+                if std::rc::Rc::ptr_eq(&right_node, &node) {
+                    break;
+                }
+                predecessor = right_node;
+            }
+            if predecessor.borrow().right.is_none() {
+                predecessor.borrow_mut().right = Some(node.clone());
+                root = node.borrow().left.clone();
+            } else {
+                predecessor.borrow_mut().right = None;
+                res.push(node.borrow().val);
+                root = node.borrow().right.clone();
+            }
+        }
+    }
+
+    res
+}
