b977295994b489f0a910c12d85802e908a655b63
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-26 21:31:03 +0800

    amazing

--- src/s114/morris.rs
+++ src/s114/morris.rs
@@ -1,12 +1,11 @@
 use crate::utils::binary_tree::*;
 
 pub fn flatten(root: &mut Tree) {
-    let mut root = root.clone();
-    while let Some(node) = root {
-        if let Some(left) = {
-            let temp = node.borrow().left.clone();
-            temp
-        } {
+    let mut cur = root.clone();
+
+    while let Some(node) = cur {
+        let mut borrowed = node.borrow_mut();
+        if let Some(left) = borrowed.left.take() {
             let mut rightmost = left.clone();
             while let Some(right) = {
                 let temp = rightmost.borrow().right.clone();
@@ -14,12 +13,33 @@ pub fn flatten(root: &mut Tree) {
             } {
                 rightmost = right;
             }
-            rightmost.borrow_mut().right = node.borrow().right.clone();
-
-            node.borrow_mut().right = Some(left);
-            node.borrow_mut().left = None;
+            rightmost.borrow_mut().right = borrowed.right.clone();
+            borrowed.right = Some(left);
         }
-
-        root = node.borrow().right.clone();
+        cur = borrowed.right.clone();
     }
 }
+
+// pub fn flatten(root: &mut Tree) {
+//     let mut root = root.clone();
+//     while let Some(node) = root {
+//         if let Some(left) = {
+//             let temp = node.borrow().left.clone();
+//             temp
+//         } {
+//             let mut rightmost = left.clone();
+//             while let Some(right) = {
+//                 let temp = rightmost.borrow().right.clone();
+//                 temp
+//             } {
+//                 rightmost = right;
+//             }
+//             rightmost.borrow_mut().right = node.borrow().right.clone();
+
+//             node.borrow_mut().right = Some(left);
+//             node.borrow_mut().left = None;
+//         }
+
+//         root = node.borrow().right.clone();
+//     }
+// }
