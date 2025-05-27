abbdd73917e00fbbc6a9cbb1f9b264e9922186c3
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-27 15:01:42 +0800

    symmetric recursive ref

--- src/s101/recursive.rs
+++ src/s101/recursive.rs
@@ -1,21 +1,19 @@
 use crate::utils::binary_tree::*;
 
 pub fn is_symmetric(root: Tree) -> bool {
-    if let Some(node) = root {
+    if let Some(node) = &root {
         let node = node.borrow();
-        recursive(node.left.clone(), node.right.clone())
+        recursive(&node.left, &node.right)
     } else {
         true
     }
 }
 
-fn recursive(l: Tree, r: Tree) -> bool {
+fn recursive(l: &Tree, r: &Tree) -> bool {
     match (l, r) {
         (Some(l), Some(r)) => {
-            let (mut l, mut r) = (l.borrow_mut(), r.borrow_mut());
-            l.val == r.val
-                && recursive(l.left.take(), r.right.take())
-                && recursive(l.right.take(), r.left.take())
+            let (l, r) = (l.borrow(), r.borrow());
+            l.val == r.val && recursive(&l.left, &r.right) && recursive(&l.right, &r.left)
         }
         (None, None) => true,
         _ => false,
