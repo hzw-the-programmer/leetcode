b89628461517e755e02a26f285727c4212e656bd
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-27 15:08:25 +0800

    s101

--- src/s101/iterative.rs
+++ src/s101/iterative.rs
@@ -1,18 +1,18 @@
 use crate::utils::binary_tree::*;
 
 pub fn is_symmetric(root: Tree) -> bool {
-    if let Some(node) = root {
-        let mut node = node.borrow_mut();
-        let mut stack = vec![(node.left.take(), node.right.clone())];
+    if let Some(node) = &root {
+        let node = node.borrow();
+        let mut stack = vec![(node.left.clone(), node.right.clone())];
         while let Some(pair) = stack.pop() {
             match pair {
                 (Some(l), Some(r)) => {
-                    let (mut l, mut r) = (l.borrow_mut(), r.borrow_mut());
+                    let (l, r) = (l.borrow(), r.borrow());
                     if l.val != r.val {
                         return false;
                     }
-                    stack.push((l.left.take(), r.right.take()));
-                    stack.push((l.right.take(), r.left.take()));
+                    stack.push((l.right.clone(), r.left.clone()));
+                    stack.push((l.left.clone(), r.right.clone()));
                 }
                 (None, None) => {}
                 _ => return false,
