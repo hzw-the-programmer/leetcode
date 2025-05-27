f39c01244906aa1e68874060f1b7b9baadbc0392
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-27 19:39:07 +0800

    better

--- src/s99/morris.rs
+++ src/s99/morris.rs
@@ -4,6 +4,7 @@ pub fn recover_tree(root: &mut Tree) {
     let mut tree = root.clone();
     let mut pre: Tree = None;
     let (mut n1, mut n2) = (None, None);
+    let (mut threaded_count, mut found) = (0, false);
 
     while let Some(node) = tree {
         if let Some(left) = node.borrow().left.clone() {
@@ -21,24 +22,32 @@ pub fn recover_tree(root: &mut Tree) {
             if rightmost.borrow().right.is_none() {
                 rightmost.borrow_mut().right = Some(node.clone());
                 tree = Some(left);
+                threaded_count += 1;
                 continue;
             } else {
                 rightmost.borrow_mut().right = None;
                 tree = node.borrow().right.clone();
+                threaded_count -= 1;
             }
         } else {
             tree = node.borrow().right.clone();
         }
 
-        if let Some(pre) = pre {
-            if pre.borrow().val > node.borrow().val {
-                n2 = Some(node.clone());
-                if n1.is_none() {
-                    n1 = Some(pre);
+        if !found {
+            if let Some(pre) = pre {
+                if pre.borrow().val > node.borrow().val {
+                    n2 = Some(node.clone());
+                    if n1.is_none() {
+                        n1 = Some(pre);
+                    } else {
+                        found = true;
+                    }
                 }
             }
+            pre = Some(node);
+        } else if threaded_count == 0 {
+            break;
         }
-        pre = Some(node);
     }
 
     match (n1, n2) {
