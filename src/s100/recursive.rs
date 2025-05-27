b90f3b52af1b7abaff4e450627ac46237c613a83
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-27 10:26:44 +0800

    better same tree

--- src/s100/recursive.rs
+++ src/s100/recursive.rs
@@ -1,18 +1,12 @@
 use crate::utils::binary_tree::*;
 
 pub fn is_same_tree(p: Tree, q: Tree) -> bool {
-    recursive(p, q)
-}
-
-fn recursive(p: Tree, q: Tree) -> bool {
     match (p, q) {
-        (Some(p_node), Some(q_node)) => {
-            if p_node.borrow().val == q_node.borrow().val {
-                recursive(p_node.borrow().left.clone(), q_node.borrow().left.clone())
-                    && recursive(p_node.borrow().right.clone(), q_node.borrow().right.clone())
-            } else {
-                false
-            }
+        (Some(p), Some(q)) => {
+            let (p, q) = (p.borrow(), q.borrow());
+            p.val == q.val
+                && is_same_tree(p.left.clone(), q.left.clone())
+                && is_same_tree(p.right.clone(), q.right.clone())
         }
         (None, None) => true,
         _ => false,
