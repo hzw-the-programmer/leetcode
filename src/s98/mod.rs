d2e5e29d48f2ac75ea418b4c09b017ca8df27dd0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-27 16:03:31 +0800

    s98

--- src/s98/mod.rs
+++ src/s98/mod.rs
@@ -1,10 +1,10 @@
 // 98. Validate Binary Search Tree
 
-mod recursive;
-pub use recursive::is_valid_bst;
+// mod recursive;
+// pub use recursive::is_valid_bst;
 
-// mod recursive2;
-// pub use recursive2::is_valid_bst;
+mod recursive2;
+pub use recursive2::is_valid_bst;
 
 // mod iterative;
 // pub use iterative::is_valid_bst;
--- src/s98/recursive2.rs
+++ src/s98/recursive2.rs
@@ -1,19 +1,19 @@
 use crate::utils::binary_tree::*;
 
 pub fn is_valid_bst(root: Tree) -> bool {
-    is_valid_bst_recursive(root, i32::MIN, i32::MAX)
+    recursive(root, i32::MIN, i32::MAX)
 }
 
-fn is_valid_bst_recursive(root: Tree, lower: i32, upper: i32) -> bool {
-    if root.is_none() {
-        return true;
+fn recursive(root: Tree, lower: i32, upper: i32) -> bool {
+    if let Some(node) = &root {
+        let node = node.borrow();
+        if node.val <= lower || node.val >= upper {
+            false
+        } else {
+            recursive(node.left.clone(), lower, node.val)
+                && recursive(node.right.clone(), node.val, upper)
+        }
+    } else {
+        true
     }
-
-    let node = root.as_ref().unwrap().borrow();
-    if node.val <= lower || node.val >= upper {
-        return false;
-    }
-
-    is_valid_bst_recursive(node.left.clone(), lower, node.val)
-        && is_valid_bst_recursive(node.right.clone(), node.val, upper)
 }
