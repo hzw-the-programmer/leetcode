e680017d4e098a208fc27e284cfb8f669221f43f
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-28 19:26:30 +0800

    min depth recursive dfs better

--- src/s111/recursive_dfs.rs
+++ src/s111/recursive_dfs.rs
@@ -8,7 +8,13 @@ pub fn min_depth(root: Tree) -> i32 {
                 (None, None) => 1,
                 (left, None) => 1 + min_depth(left),
                 (None, right) => 1 + min_depth(right),
-                (left, right) => 1 + min_depth(left).min(min_depth(right)),
+                (left, right) => {
+                    let mut d = min_depth(left);
+                    if d > 1 {
+                        d = d.min(min_depth(right));
+                    }
+                    1 + d
+                }
             }
         }
         None => 0,
