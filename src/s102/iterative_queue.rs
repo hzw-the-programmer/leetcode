1a4aa9870cc335cf8354deb7a4e3c9f178c2f308
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-28 08:28:11 +0800

    iterative stack

--- src/s102/iterative_queue.rs
+++ src/s102/iterative_queue.rs
@@ -26,23 +26,3 @@ pub fn level_order(root: Tree) -> Vec<Vec<i32>> {
 
     res
 }
-
-// pub fn level_order(root: Tree) -> Vec<Vec<i32>> {
-//     let mut res: Vec<Vec<i32>> = vec![];
-
-//     let mut stack = vec![(0, root)];
-//     while let Some((l, tree)) = stack.pop() {
-//         if let Some(node) = tree {
-//             let node = node.borrow();
-//             if l < res.len() {
-//                 res[l].push(node.val);
-//             } else {
-//                 res.push(vec![node.val]);
-//             }
-//             stack.push((l + 1, node.right.clone()));
-//             stack.push((l + 1, node.left.clone()));
-//         }
-//     }
-
-//     res
-// }
--- src/s102/mod.rs
+++ src/s102/mod.rs
@@ -1,7 +1,10 @@
 // 102. Binary Tree Level Order Traversal
 
-mod iterative_queue;
-pub use iterative_queue::level_order;
+// mod iterative_queue;
+// pub use iterative_queue::level_order;
+
+mod iterative_stack;
+pub use iterative_stack::level_order;
 
 // mod recursive;
 // pub use recursive::level_order;
