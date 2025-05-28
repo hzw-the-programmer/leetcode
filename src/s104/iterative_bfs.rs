1ac32642decd78118d15fa6cecc3b2afd1d3dd16
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-28 17:36:21 +0800

    max depth iterative bfs

---
+++ src/s104/iterative_bfs.rs
@@ -0,0 +1,25 @@
+use crate::utils::binary_tree::*;
+
+pub fn max_depth(root: Tree) -> i32 {
+    if let Some(node) = root {
+        let mut res = 0;
+        let mut queue = std::collections::VecDeque::new();
+        queue.push_back(node);
+        while !queue.is_empty() {
+            res += 1;
+            let n = queue.len();
+            for _ in 0..n {
+                let node = queue.pop_front().unwrap();
+                if node.borrow().left.is_some() {
+                    queue.push_front(node.borrow().left.clone().unwrap());
+                }
+                if node.borrow().right.is_some() {
+                    queue.push_front(node.borrow().right.clone().unwrap());
+                }
+            }
+        }
+        res
+    } else {
+        0
+    }
+}
--- src/s104/mod.rs
+++ src/s104/mod.rs
@@ -1,7 +1,10 @@
 // 104. Maximum Depth of Binary Tree
 
-mod recursive;
-pub use recursive::max_depth;
+// mod recursive;
+// pub use recursive::max_depth;
+
+mod iterative_bfs;
+pub use iterative_bfs::max_depth;
 
 #[cfg(test)]
 mod tests;
