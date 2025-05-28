78e2fc364bb78e2204b7ca280e763351f2cdb916
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-28 15:02:36 +0800

    option array

--- src/utils/binary_tree/tests.rs
+++ src/utils/binary_tree/tests.rs
@@ -1,7 +1,13 @@
 #[test]
-fn test_1() {
+fn test_option_array_1() {
     assert_eq!(
         option_array!([3, 9, 20, null, null, 15, 7]),
         [Some(3), Some(9), Some(20), None, None, Some(15), Some(7)]
     );
+}
+
+#[test]
+fn test_option_array_2() {
+    let res: [Option<i32>; 0] = option_array!([]);
+    assert_eq!(res, []);
 }
