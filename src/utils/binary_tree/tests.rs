3190ed903152993834bc6e39b96e426c967678bc
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-30 08:12:33 +0800

    some_array! works

--- src/utils/binary_tree/tests.rs
+++ src/utils/binary_tree/tests.rs
@@ -16,10 +16,10 @@ fn test_option_array_2() {
 
 #[test]
 fn test_option_array_3() {
-    // assert_eq!(
-    //     some_array!([3, -9, 20, null, null, 15, 7]),
-    //     [Some(3), Some(-9), Some(20), None, None, Some(15), Some(7)]
-    // );
+    assert_eq!(
+        some_array!([3, -9, 20, null, null, 15, 7]),
+        [Some(3), Some(-9), Some(20), None, None, Some(15), Some(7)]
+    );
     assert_eq!(some_array!([1, null]), [Some(1), None]);
     assert_eq!(some_array!([-1, null]), [Some(-1), None]);
 }
