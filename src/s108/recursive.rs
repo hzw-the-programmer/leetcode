93244382b5b6bb1ef90865fd76794702757c21af
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-30 09:25:06 +0800

    if even, select left of mid

--- src/s108/recursive.rs
+++ src/s108/recursive.rs
@@ -1,22 +1,36 @@
 use crate::utils::binary_tree::*;
 
 pub fn sorted_array_to_bst(nums: Vec<i32>) -> Tree {
-    recursive(&nums, 0, nums.len() - 1)
+    // recursive(&nums, 0, nums.len() - 1)
+    recursive(&nums)
 }
 
-fn recursive(nums: &[i32], lower: usize, upper: usize) -> Tree {
-    if lower > upper {
-        return None;
-    }
+// fn recursive(nums: &[i32], lower: usize, upper: usize) -> Tree {
+//     if lower > upper {
+//         return None;
+//     }
 
-    let mid = (lower + upper) / 2;
-    new(
-        nums[mid],
-        if mid < 1 {
-            None
-        } else {
-            recursive(nums, lower, mid - 1)
-        },
-        recursive(nums, mid + 1, upper),
-    )
+//     let mid = (lower + upper) / 2;
+//     new(
+//         nums[mid],
+//         if mid < 1 {
+//             None
+//         } else {
+//             recursive(nums, lower, mid - 1)
+//         },
+//         recursive(nums, mid + 1, upper),
+//     )
+// }
+
+fn recursive(nums: &[i32]) -> Tree {
+    if nums.is_empty() {
+        None
+    } else {
+        let mid = (nums.len() - 1) / 2;
+        new(
+            nums[mid],
+            recursive(&nums[..mid]),
+            recursive(&nums[mid + 1..]),
+        )
+    }
 }
