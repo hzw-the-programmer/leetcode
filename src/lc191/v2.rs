f35c39af8d96fd2afc4e6ee6b6053a3b9c29882c
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-14 16:04:40 +0800

    another flag implementation

--- src/lc191/v2.rs
+++ src/lc191/v2.rs
@@ -1,11 +1,21 @@
+// pub fn hamming_weight(n: i32) -> i32 {
+//     let mut count = 0;
+//     let mut flag = 1;
+//     while flag != 0 {
+//         if n & flag == flag {
+//             count += 1;
+//         }
+//         flag <<= 1;
+//     }
+//     count
+// }
+
 pub fn hamming_weight(n: i32) -> i32 {
     let mut count = 0;
-    let mut flag = 1;
-    while flag != 0 {
-        if n & flag == flag {
+    for i in 0..32 {
+        if n & 1 << i != 0 {
             count += 1;
         }
-        flag <<= 1;
     }
     count
 }
