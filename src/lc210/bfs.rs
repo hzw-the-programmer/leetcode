709c29d6cb274edf5d94362665258d99316740c0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-14 15:10:22 +0800

    course schedule faster

--- src/lc210/bfs.rs
+++ src/lc210/bfs.rs
@@ -1,8 +1,10 @@
 pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
     let num_courses = num_courses as usize;
     let mut in_degree = vec![0; num_courses];
+    let mut outs = vec![vec![]; num_courses];
     for pair in &prerequisites {
         in_degree[pair[0] as usize] += 1;
+        outs[pair[1] as usize].push(pair[0] as usize);
     }
     let mut queue = std::collections::VecDeque::with_capacity(num_courses);
     let mut res = Vec::with_capacity(num_courses);
@@ -13,12 +15,10 @@ pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
     }
     while let Some(i) = queue.pop_front() {
         res.push(i as i32);
-        for pair in &prerequisites {
-            if i == pair[1] as usize {
-                in_degree[pair[0] as usize] -= 1;
-                if in_degree[pair[0] as usize] == 0 {
-                    queue.push_back(pair[0] as usize);
-                }
+        for &j in &outs[i] {
+            in_degree[j] -= 1;
+            if in_degree[j] == 0 {
+                queue.push_back(j);
             }
         }
     }
