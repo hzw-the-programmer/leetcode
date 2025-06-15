f5ed1d1cfecd2235dadf68455b4bc0e6856d3374
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-15 15:37:32 +0800

    path sum recursive better

--- src/lc112/recursive.rs
+++ src/lc112/recursive.rs
@@ -3,19 +3,19 @@ use core::cell::RefCell;
 use std::rc::Rc;
 
 pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
-    recursive(root.as_deref(), target_sum, 0)
+    recursive(root.as_deref(), target_sum)
 }
 
-fn recursive(root: Option<&RefCell<TreeNode>>, target_sum: i32, sum: i32) -> bool {
+fn recursive(root: Option<&RefCell<TreeNode>>, target_sum: i32) -> bool {
     match root {
         None => false,
         Some(node) => {
             let node = node.borrow();
-            if node.left.is_none() && node.right.is_none() && node.val + sum == target_sum {
-                true
+            if node.left.is_none() && node.right.is_none() {
+                node.val == target_sum
             } else {
-                recursive(node.left.as_deref(), target_sum, node.val + sum)
-                    || recursive(node.right.as_deref(), target_sum, node.val + sum)
+                recursive(node.left.as_deref(), target_sum - node.val)
+                    || recursive(node.right.as_deref(), target_sum - node.val)
             }
         }
     }
