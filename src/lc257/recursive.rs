9cef4def4ac636e7ddb5b3b1c9e928272d4741a3
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-17 17:34:35 +0800

    not use write!

--- src/lc257/recursive.rs
+++ src/lc257/recursive.rs
@@ -1,6 +1,5 @@
 use crate::utils::binary_tree::TreeNode;
 use core::cell::RefCell;
-use core::fmt::Write;
 use std::rc::Rc;
 
 pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
@@ -14,13 +13,13 @@ fn recursive(root: Option<&RefCell<TreeNode>>, temp: &mut String, res: &mut Vec<
     if let Some(node) = root {
         let node = node.borrow();
         let len = temp.len();
-        let _ = write!(temp, "{}", node.val);
+        temp.push_str(&node.val.to_string());
         match (node.left.as_deref(), node.right.as_deref()) {
             (None, None) => res.push(temp.clone()),
             (left, right) => {
                 for node in left.into_iter().chain(right) {
                     let len = temp.len();
-                    let _ = write!(temp, "->");
+                    temp.push_str("->");
                     recursive(Some(node), temp, res);
                     temp.truncate(len);
                 }
