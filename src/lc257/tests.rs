a3cc6b373bd5ecb370a6bd5633c28bd69415ca66
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-21 22:49:41 +0800

    refactor tests

--- src/lc257/tests.rs
+++ src/lc257/tests.rs
@@ -3,9 +3,11 @@ use crate::utils::binary_tree::btree;
 
 #[test]
 fn t1() {
-    assert_eq!(
-        binary_tree_paths(btree![1, 2, 3, null, 5]),
-        vec!["1->2->5", "1->3"]
-    );
-    assert_eq!(binary_tree_paths(btree![1]), vec!["1"]);
+    let tests = [
+        (btree![1, 2, 3, null, 5], vec!["1->2->5", "1->3"]),
+        (btree![1], vec!["1"]),
+    ];
+    for (i, test) in tests.iter().enumerate() {
+        assert_eq!(binary_tree_paths(test.0.clone()), test.1, "{}", i);
+    }
 }
