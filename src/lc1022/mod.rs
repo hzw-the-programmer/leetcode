0d36f040d7804efae4f4103cd3c947d3a0ff83c4
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-22 13:50:20 +0800

    preorder dfs recursive

--- src/lc1022/mod.rs
+++ src/lc1022/mod.rs
@@ -1,7 +1,7 @@
 // 1022. Sum of Root To Leaf Binary Numbers
 
-mod recursive;
-pub use recursive::sum_root_to_leaf;
+mod preorder_dfs_recursive;
+pub use preorder_dfs_recursive::sum_root_to_leaf;
 
 #[cfg(test)]
 mod tests;
