4857ce62280485cc6c648168c3c1165c913915fa
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-22 08:16:44 +0800

    refactor

--- src/lc257/preorder_dfs_recursive.rs
+++ src/lc257/preorder_dfs_recursive.rs
@@ -4,27 +4,52 @@ use std::rc::Rc;
 
 pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
     let mut res = vec![];
-    let mut temp = String::new();
-    recursive(root.as_deref(), &mut temp, &mut res);
+    recursive(root.as_deref(), String::new(), &mut res);
     res
 }
 
-fn recursive(root: Option<&RefCell<TreeNode>>, temp: &mut String, res: &mut Vec<String>) {
+fn recursive(root: Option<&RefCell<TreeNode>>, mut path: String, res: &mut Vec<String>) {
     if let Some(node) = root {
         let node = node.borrow();
-        let len = temp.len();
-        temp.push_str(&node.val.to_string());
+        path.push_str(&node.val.to_string());
         match (node.left.as_deref(), node.right.as_deref()) {
-            (None, None) => res.push(temp.clone()),
-            (left, right) => {
-                for node in left.into_iter().chain(right) {
-                    let len = temp.len();
-                    temp.push_str("->");
-                    recursive(Some(node), temp, res);
-                    temp.truncate(len);
-                }
+            (None, None) => res.push(path),
+            (Some(node), None) | (None, Some(node)) => {
+                path.push_str("->");
+                recursive(Some(node), path, res);
+            }
+            (Some(left), Some(right)) => {
+                path.push_str("->");
+                recursive(Some(left), path.clone(), res);
+                recursive(Some(right), path, res);
             }
         }
-        temp.truncate(len);
     }
 }
+
+// pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
+//     let mut res = vec![];
+//     let mut temp = String::new();
+//     recursive(root.as_deref(), &mut temp, &mut res);
+//     res
+// }
+
+// fn recursive(root: Option<&RefCell<TreeNode>>, temp: &mut String, res: &mut Vec<String>) {
+//     if let Some(node) = root {
+//         let node = node.borrow();
+//         let len = temp.len();
+//         temp.push_str(&node.val.to_string());
+//         match (node.left.as_deref(), node.right.as_deref()) {
+//             (None, None) => res.push(temp.clone()),
+//             (left, right) => {
+//                 for node in left.into_iter().chain(right) {
+//                     let len = temp.len();
+//                     temp.push_str("->");
+//                     recursive(Some(node), temp, res);
+//                     temp.truncate(len);
+//                 }
+//             }
+//         }
+//         temp.truncate(len);
+//     }
+// }
