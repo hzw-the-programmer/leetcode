a4374f7f8a26b6306cc202c9016a9791b1a36fca
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-22 08:48:59 +0800

    don't modify test data

--- src/lc226/tests.rs
+++ src/lc226/tests.rs
@@ -3,10 +3,17 @@ use crate::utils::binary_tree::btree;
 
 #[test]
 fn t1() {
-    assert_eq!(
-        invert_tree(btree![4, 2, 7, 1, 3, 6, 9]),
-        btree![4, 7, 2, 9, 6, 3, 1]
-    );
-    assert_eq!(invert_tree(btree![2, 1, 3]), btree![2, 3, 1]);
-    assert_eq!(invert_tree(btree![]), btree![]);
+    let tests = [
+        (btree![4, 2, 7, 1, 3, 6, 9], btree![4, 7, 2, 9, 6, 3, 1]),
+        (btree![2, 1, 3], btree![2, 3, 1]),
+        (btree![], btree![]),
+    ];
+
+    for (i, test) in tests.iter().enumerate() {
+        assert_eq!(invert_tree(test.0.clone()), test.1, "{}", i);
+    }
+
+    for (i, test) in tests.iter().enumerate() {
+        assert_eq!(invert_tree(test.1.clone()), test.0, "{}", i);
+    }
 }
