0de2d675686b0ca739506b86d9ee36896b59045b
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-22 08:31:08 +0800

    refactor

--- src/lc257/mod.rs
+++ src/lc257/mod.rs
@@ -1,10 +1,10 @@
 // 257. Binary Tree Paths
 
-mod preorder_dfs_recursive;
-pub use preorder_dfs_recursive::binary_tree_paths;
+// mod preorder_dfs_recursive;
+// pub use preorder_dfs_recursive::binary_tree_paths;
 
-// mod preorder_dfs_iterative;
-// pub use preorder_dfs_iterative::binary_tree_paths;
+mod preorder_dfs_iterative;
+pub use preorder_dfs_iterative::binary_tree_paths;
 
 // mod levelorder_bfs_iterative;
 // pub use levelorder_bfs_iterative::binary_tree_paths;
--- src/lc257/preorder_dfs_iterative.rs
+++ src/lc257/preorder_dfs_iterative.rs
@@ -4,25 +4,31 @@ use std::rc::Rc;
 
 pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
     let mut res = vec![];
-    let mut temp = String::new();
     match root {
         None => {}
         Some(node) => {
-            let mut stack = vec![(node, 0)];
-            while let Some((node, len)) = stack.pop() {
-                temp.truncate(len);
+            let mut stack = vec![(node, String::new())];
+            while let Some((node, mut path)) = stack.pop() {
                 let mut current = Some(node);
                 while let Some(node) = current.take() {
                     let node = node.borrow();
-                    temp.push_str(&node.val.to_string());
+                    path.push_str(&node.val.to_string());
                     match (node.left.clone(), node.right.clone()) {
-                        (None, None) => res.push(temp.clone()),
-                        (left, right) => {
-                            temp.push_str("->");
-                            current = left;
+                        (None, right) => {
                             if let Some(node) = right {
-                                stack.push((node, temp.len()));
+                                path.push_str("->");
+                                stack.push((node, path));
+                            } else {
+                                res.push(path);
                             }
+                            break;
+                        }
+                        (Some(left), right) => {
+                            path.push_str("->");
+                            if let Some(node) = right {
+                                stack.push((node, path.clone()));
+                            }
+                            current = Some(left);
                         }
                     }
                 }
@@ -31,3 +37,33 @@ pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
     }
     res
 }
+
+// pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
+//     let mut res = vec![];
+//     let mut temp = String::new();
+//     match root {
+//         None => {}
+//         Some(node) => {
+//             let mut stack = vec![(node, 0)];
+//             while let Some((node, len)) = stack.pop() {
+//                 temp.truncate(len);
+//                 let mut current = Some(node);
+//                 while let Some(node) = current.take() {
+//                     let node = node.borrow();
+//                     temp.push_str(&node.val.to_string());
+//                     match (node.left.clone(), node.right.clone()) {
+//                         (None, None) => res.push(temp.clone()),
+//                         (left, right) => {
+//                             temp.push_str("->");
+//                             current = left;
+//                             if let Some(node) = right {
+//                                 stack.push((node, temp.len()));
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     res
+// }
