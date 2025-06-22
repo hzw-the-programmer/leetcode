5341e4e634fc9981caf42ba070f3ba975a260b65
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-22 22:02:40 +0800

    refactor

--- src/lc230/inorder_dfs_recursive.rs
+++ src/lc230/inorder_dfs_recursive.rs
@@ -4,25 +4,25 @@ use std::rc::Rc;
 
 pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
     let mut res = 0;
-    let mut len = 0;
-    recursive(root.as_deref(), k, &mut len, &mut res);
+    let mut k = k;
+    recursive(root.as_deref(), &mut k, &mut res);
     res
 }
 
-fn recursive(root: Option<&RefCell<TreeNode>>, k: i32, len: &mut i32, res: &mut i32) {
+fn recursive(root: Option<&RefCell<TreeNode>>, k: &mut i32, res: &mut i32) {
     match root {
         None => {}
         Some(node) => {
             let node = node.borrow();
-            recursive(node.left.as_deref(), k, len, res);
-            *len += 1;
-            if *len == k {
+            recursive(node.left.as_deref(), k, res);
+            *k -= 1;
+            if *k == 0 {
                 *res = node.val;
                 return;
-            } else if *len > k {
+            } else if *k < 0 {
                 return;
             }
-            recursive(node.right.as_deref(), k, len, res);
+            recursive(node.right.as_deref(), k, res);
         }
     }
 }
