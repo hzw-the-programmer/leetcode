c659a4993cb36598a546b51adbc99549b37ea5cb
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-23 08:39:36 +0800

    refactor

--- src/lc230/inorder_dfs_iterative.rs
+++ src/lc230/inorder_dfs_iterative.rs
@@ -2,57 +2,53 @@ use crate::utils::binary_tree::TreeNode;
 use core::cell::RefCell;
 use std::rc::Rc;
 
-enum TraversalStep {
-    First(Rc<RefCell<TreeNode>>),
-    Second(i32),
-}
+// enum TraversalStep {
+//     First(Rc<RefCell<TreeNode>>),
+//     Second(i32),
+// }
 
-use TraversalStep::*;
+// use TraversalStep::*;
 
-pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
-    let mut idx = 0;
-    let mut stack = Vec::from_iter(root.map(First));
-    while let Some(step) = stack.pop() {
-        match step {
-            First(node) => {
-                let node = node.borrow();
-                stack.extend(node.right.clone().map(First));
-                stack.push(Second(node.val));
-                stack.extend(node.left.clone().map(First));
-            }
-            Second(val) => {
-                idx += 1;
-                if idx == k {
-                    return val;
-                }
-            }
-        }
-    }
-    unreachable!()
-}
-
-// pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, mut k: i32) -> i32 {
-//     match root {
-//         None => {}
-//         Some(node) => {
-//             let mut stack = vec![node.clone()];
-//             let mut current = Some(node);
-//             while !stack.is_empty() || current.is_some() {
-//                 while let Some(node) = current {
-//                     stack.push(node.clone());
-//                     current = node.borrow().left.clone();
-//                 }
-
-//                 if let Some(node) = stack.pop() {
-//                     let node = node.borrow();
-//                     k -= 1;
-//                     if k == 0 {
-//                         return node.val;
-//                     }
-//                     current = node.right.clone();
+// pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+//     let mut idx = 0;
+//     let mut stack = Vec::from_iter(root.map(First));
+//     while let Some(step) = stack.pop() {
+//         match step {
+//             First(node) => {
+//                 let node = node.borrow();
+//                 stack.extend(node.right.clone().map(First));
+//                 stack.push(Second(node.val));
+//                 stack.extend(node.left.clone().map(First));
+//             }
+//             Second(val) => {
+//                 idx += 1;
+//                 if idx == k {
+//                     return val;
 //                 }
 //             }
 //         }
 //     }
-//     0
+//     unreachable!()
 // }
+
+pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+    let mut idx = 0;
+    let mut current = root;
+    let mut stack = vec![];
+    while current.is_some() || !stack.is_empty() {
+        while let Some(node) = current {
+            current = node.borrow().left.clone();
+            stack.push(node);
+        }
+
+        if let Some(node) = stack.pop() {
+            let node = node.borrow();
+            idx += 1;
+            if idx == k {
+                return node.val;
+            }
+            current = node.right.clone();
+        }
+    }
+    unreachable!()
+}
