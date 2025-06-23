86e11ce10602edaed0e917bbe85467528f917144
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-23 08:27:56 +0800

    inorder traversal First, Second

--- src/lc230/inorder_dfs_iterative.rs
+++ src/lc230/inorder_dfs_iterative.rs
@@ -2,28 +2,57 @@ use crate::utils::binary_tree::TreeNode;
 use core::cell::RefCell;
 use std::rc::Rc;
 
-pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, mut k: i32) -> i32 {
-    match root {
-        None => {}
-        Some(node) => {
-            let mut stack = vec![node.clone()];
-            let mut current = Some(node);
-            while !stack.is_empty() || current.is_some() {
-                while let Some(node) = current {
-                    stack.push(node.clone());
-                    current = node.borrow().left.clone();
-                }
+enum TraversalStep {
+    First(Rc<RefCell<TreeNode>>),
+    Second(i32),
+}
 
-                if let Some(node) = stack.pop() {
-                    let node = node.borrow();
-                    k -= 1;
-                    if k == 0 {
-                        return node.val;
-                    }
-                    current = node.right.clone();
+use TraversalStep::*;
+
+pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+    let mut idx = 0;
+    let mut stack = Vec::from_iter(root.map(First));
+    while let Some(step) = stack.pop() {
+        match step {
+            First(node) => {
+                let node = node.borrow();
+                stack.extend(node.right.clone().map(First));
+                stack.push(Second(node.val));
+                stack.extend(node.left.clone().map(First));
+            }
+            Second(val) => {
+                idx += 1;
+                if idx == k {
+                    return val;
                 }
             }
         }
     }
-    0
+    unreachable!()
 }
+
+// pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, mut k: i32) -> i32 {
+//     match root {
+//         None => {}
+//         Some(node) => {
+//             let mut stack = vec![node.clone()];
+//             let mut current = Some(node);
+//             while !stack.is_empty() || current.is_some() {
+//                 while let Some(node) = current {
+//                     stack.push(node.clone());
+//                     current = node.borrow().left.clone();
+//                 }
+
+//                 if let Some(node) = stack.pop() {
+//                     let node = node.borrow();
+//                     k -= 1;
+//                     if k == 0 {
+//                         return node.val;
+//                     }
+//                     current = node.right.clone();
+//                 }
+//             }
+//         }
+//     }
+//     0
+// }
