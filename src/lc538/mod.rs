c7dbadde13a81fb68587d908650ca51e0e0d21f9
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-24 11:05:44 +0800

    refactor

--- src/lc538/mod.rs
+++ src/lc538/mod.rs
@@ -1,13 +1,13 @@
 // 538. Convert BST to Greater Tree
 
-// mod inorder_rev_dfs_recursive;
-// pub use inorder_rev_dfs_recursive::convert_bst;
+mod reverse_inorder_dfs_recursive;
+pub use reverse_inorder_dfs_recursive::convert_bst;
 
-// mod inorder_rev_dfs_iterative;
-// pub use inorder_rev_dfs_iterative::convert_bst;
+// mod reverse_inorder_dfs_iterative;
+// pub use reverse_inorder_dfs_iterative::convert_bst;
 
-mod inorder_rev_dfs_morris;
-pub use inorder_rev_dfs_morris::convert_bst;
+// mod reverse_inorder_dfs_morris;
+// pub use reverse_inorder_dfs_morris::convert_bst;
 
 #[cfg(test)]
 mod tests;
