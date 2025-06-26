c3e636832bdb669bb5d07b88f4c1161bb3a77542
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-26 15:48:47 +0800

    refactor

--- src/lc404/inorder_dfs_recursive.rs
+++ src/lc404/inorder_dfs_recursive.rs
@@ -3,21 +3,39 @@ use core::cell::RefCell;
 use std::rc::Rc;
 
 pub fn sum_of_left_leaves(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    let mut res = 0;
-    match root {
-        None => {}
-        Some(root) => {
-            let root = root.borrow();
-            if let Some(node) = root.left.clone() {
+    fn recursive(node: Option<&RefCell<TreeNode>>, is_left: bool) -> i32 {
+        match node {
+            None => 0,
+            Some(node) => {
                 let node = node.borrow();
-                if node.left.is_none() && node.right.is_none() {
-                    res += node.val;
+                if is_left && node.left.is_none() && node.right.is_none() {
+                    node.val
                 } else {
-                    res += sum_of_left_leaves(root.left.clone());
+                    recursive(node.left.as_deref(), true) + recursive(node.right.as_deref(), false)
                 }
             }
-            res += sum_of_left_leaves(root.right.clone());
         }
     }
-    res
+
+    recursive(root.as_deref(), false)
 }
+
+// pub fn sum_of_left_leaves(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+//     let mut res = 0;
+//     match root {
+//         None => {}
+//         Some(root) => {
+//             let root = root.borrow();
+//             if let Some(node) = root.left.clone() {
+//                 let node = node.borrow();
+//                 if node.left.is_none() && node.right.is_none() {
+//                     res += node.val;
+//                 } else {
+//                     res += sum_of_left_leaves(root.left.clone());
+//                 }
+//             }
+//             res += sum_of_left_leaves(root.right.clone());
+//         }
+//     }
+//     res
+// }
