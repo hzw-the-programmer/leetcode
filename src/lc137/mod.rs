2a05a3308a1285388ee6c1426cab66de23ccdc19
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-28 22:43:51 +0800

    optimized

--- src/lc137/mod.rs
+++ src/lc137/mod.rs
@@ -1,15 +1,22 @@
 // 137. Single Number II
 
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    let mut res = 0;
-    for i in 0..32 {
-        let total = nums.iter().fold(0, |acc, &n| acc + (n as u32 >> i & 1));
-        if total % 3 != 0 {
-            res |= 1 << i;
-        }
-    }
-    res
+    (0..32).fold(0, |acc, i| {
+        // acc | (nums.iter().fold(0, |acc, &n| acc + (n as u32 >> i & 1)) % 3 << i) as i32
+        acc | nums.iter().fold(0, |acc, &n| acc + (n >> i & 1)) % 3 << i
+    })
 }
+
+// pub fn single_number(nums: Vec<i32>) -> i32 {
+//     let mut res = 0;
+//     for i in 0..32 {
+//         let total = nums.iter().fold(0, |acc, &n| acc + (n as u32 >> i & 1));
+//         if total % 3 != 0 {
+//             res |= 1 << i;
+//         }
+//     }
+//     res
+// }
 
 // pub fn single_number(nums: Vec<i32>) -> i32 {
 //     nums.iter()
@@ -33,7 +40,11 @@ mod tests {
 
     #[test]
     fn t1() {
-        let tests = [(vec![2, 2, 3, 2], 3), (vec![0, 1, 0, 1, 0, 1, 99], 99)];
+        let tests = [
+            (vec![2, 2, 3, 2], 3),
+            (vec![0, 1, 0, 1, 0, 1, 99], 99),
+            (vec![-1, -1, -1, -2, -2, -2, 3], 3),
+        ];
 
         for (i, test) in tests.iter().enumerate() {
             assert_eq!(single_number(test.0.clone()), test.1, "{}", i);
