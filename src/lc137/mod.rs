f29d46e6cc9fd4ff89228e09d0a4ceaca5df2321
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-28 22:17:06 +0800

    optimized

--- src/lc137/mod.rs
+++ src/lc137/mod.rs
@@ -1,19 +1,14 @@
 // 137. Single Number II
 
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    nums.iter()
-        .fold(vec![0; 32], |mut acc, &n| {
-            for (i, e) in acc.iter_mut().enumerate() {
-                *e += (n as u32 & (1 << i)) >> i;
-            }
-            acc
-        })
-        .iter()
-        .enumerate()
-        .fold(0, |mut acc, (i, n)| {
-            acc |= ((n % 3) << i) as i32;
-            acc
-        })
+    let mut res = 0;
+    for i in 0..32 {
+        let total = nums.iter().fold(0, |acc, &n| acc + (n as u32 >> i & 1));
+        if total % 3 != 0 {
+            res |= 1 << i;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
