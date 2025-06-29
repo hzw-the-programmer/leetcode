6c2b070f3eb0ae67cd33bacb0a367f41e90be9bd
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-29 17:49:26 +0800

    use dummy node

--- src/lcr136/iterative.rs
+++ src/lcr136/iterative.rs
@@ -1,30 +1,21 @@
 use crate::utils::singly_linked_list::ListNode;
 
 pub fn delete_node(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
-    match head {
-        None => None,
-        Some(mut head) => {
-            if val == head.val {
-                head.next.take()
-            } else {
-                let mut current = head.as_mut();
-                loop {
-                    let next = current.next.take();
-                    match next {
-                        None => break,
-                        Some(mut node) => {
-                            if node.val == val {
-                                current.next = node.next.take();
-                                break;
-                            } else {
-                                current.next = Some(node);
-                                current = current.next.as_mut().unwrap();
-                            }
-                        }
-                    }
+    let mut dummy = Box::new(ListNode { val: 0, next: head });
+    let mut current = dummy.as_mut();
+    loop {
+        let next = current.next.take();
+        match next {
+            None => break,
+            Some(mut node) => {
+                if node.val == val {
+                    current.next = node.next.take();
+                    break;
                 }
-                Some(head)
+                current.next = Some(node);
+                current = current.next.as_mut().unwrap();
             }
         }
     }
+    dummy.next
 }
