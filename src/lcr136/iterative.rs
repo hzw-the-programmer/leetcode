2f0ef8a451c0d8c9d90174bb413703bb280ad339
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-29 17:50:55 +0800

    if val is T, we can't use dummy node

--- src/lcr136/iterative.rs
+++ src/lcr136/iterative.rs
@@ -19,3 +19,32 @@ pub fn delete_node(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode
     }
     dummy.next
 }
+
+// pub fn delete_node(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+//     match head {
+//         None => None,
+//         Some(mut head) => {
+//             if val == head.val {
+//                 head.next.take()
+//             } else {
+//                 let mut current = head.as_mut();
+//                 loop {
+//                     let next = current.next.take();
+//                     match next {
+//                         None => break,
+//                         Some(mut node) => {
+//                             if node.val == val {
+//                                 current.next = node.next.take();
+//                                 break;
+//                             } else {
+//                                 current.next = Some(node);
+//                                 current = current.next.as_mut().unwrap();
+//                             }
+//                         }
+//                     }
+//                 }
+//                 Some(head)
+//             }
+//         }
+//     }
+// }
