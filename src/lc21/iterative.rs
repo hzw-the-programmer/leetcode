c27e948fdcc31182a2020415ec2ee0f18fc0eea3
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-30 17:08:56 +0800

    dummy node

--- src/lc21/iterative.rs
+++ src/lc21/iterative.rs
@@ -1,5 +1,34 @@
 use crate::utils::singly_linked_list::ListNode;
 
+// pub fn merge_two_lists(
+//     list1: Option<Box<ListNode>>,
+//     list2: Option<Box<ListNode>>,
+// ) -> Option<Box<ListNode>> {
+//     let (mut list1, mut list2) = (list1, list2);
+//     let mut dummy = ListNode::new(0);
+//     let mut current = &mut dummy;
+
+//     while let (Some(node1), Some(node2)) = (list1.as_ref(), list2.as_ref()) {
+//         if node1.val < node2.val {
+//             current.next = list1.take();
+//             current = &mut **current.next.as_mut().unwrap();
+//             list1 = current.next.take();
+//         } else {
+//             current.next = list2.take();
+//             current = &mut **current.next.as_mut().unwrap();
+//             list2 = current.next.take();
+//         }
+//     }
+
+//     if list1.is_some() {
+//         current.next = list1.take();
+//     } else {
+//         current.next = list2.take();
+//     }
+
+//     dummy.next
+// }
+
 pub fn merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
@@ -7,16 +36,17 @@ pub fn merge_two_lists(
     let (mut list1, mut list2) = (list1, list2);
     let mut res = None;
     let mut current = &mut res;
-    
+
     while let (Some(node1), Some(node2)) = (list1.as_ref(), list2.as_ref()) {
         if node1.val < node2.val {
             *current = list1.take();
-            list1 = (*current).as_mut().unwrap().next.take();
+            current = &mut (*current).as_mut().unwrap().next;
+            list1 = (*current).take();
         } else {
             *current = list2.take();
-            list2 = (*current).as_mut().unwrap().next.take();
+            current = &mut (*current).as_mut().unwrap().next;
+            list2 = (*current).take();
         }
-        current = &mut (*current).as_mut().unwrap().next;
     }
 
     if list1.is_some() {
