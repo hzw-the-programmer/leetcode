8455c10fff7ba1cc7c69c6b46ca10974c071955a
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-30 21:12:39 +0800

    “alternative”

--- src/lc83/mod.rs
+++ src/lc83/mod.rs
@@ -3,19 +3,35 @@
 use crate::utils::singly_linked_list::ListNode;
 
 pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut current = head.as_mut();
-    while let Some(cur) = current.take() {
-        while let Some(next) = cur.next.as_mut() {
-            if cur.val == next.val {
-                cur.next = next.next.take();
-            } else {
-                break;
-            }
+    if head.is_none() {
+        return None;
+    }
+
+    let mut current = head.as_mut().unwrap();
+    while let Some(next) = current.next.as_mut() {
+        if current.val == next.val {
+            current.next = next.next.take();
+        } else {
+            current = current.next.as_mut().unwrap();
         }
-        current = cur.next.as_mut();
     }
     head
 }
+
+// pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     let mut current = head.as_mut();
+//     while let Some(cur) = current.take() {
+//         while let Some(next) = cur.next.as_mut() {
+//             if cur.val == next.val {
+//                 cur.next = next.next.take();
+//             } else {
+//                 break;
+//             }
+//         }
+//         current = cur.next.as_mut();
+//     }
+//     head
+// }
 
 // pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
 //     let mut current = head.as_mut();
