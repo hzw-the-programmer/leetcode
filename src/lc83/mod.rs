5128fc453ae591222e3c5dee1ac829af32ac6a5d
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-30 20:50:29 +0800

    alternative

--- src/lc83/mod.rs
+++ src/lc83/mod.rs
@@ -5,17 +5,32 @@ use crate::utils::singly_linked_list::ListNode;
 pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut current = head.as_mut();
     while let Some(cur) = current.take() {
-        if let Some(next) = cur.next.as_mut() {
+        while let Some(next) = cur.next.as_mut() {
             if cur.val == next.val {
                 cur.next = next.next.take();
-                current = Some(cur);
             } else {
-                current = cur.next.as_mut();
+                break;
             }
         }
+        current = cur.next.as_mut();
     }
     head
 }
+
+// pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     let mut current = head.as_mut();
+//     while let Some(cur) = current.take() {
+//         if let Some(next) = cur.next.as_mut() {
+//             if cur.val == next.val {
+//                 cur.next = next.next.take();
+//                 current = Some(cur);
+//             } else {
+//                 current = cur.next.as_mut();
+//             }
+//         }
+//     }
+//     head
+// }
 
 #[cfg(test)]
 mod tests;
