e90dacb493be63441f8a6e42eae405fd113e73e0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-01 15:21:15 +0800

    don't use dummy node

--- src/lc203/iterative.rs
+++ src/lc203/iterative.rs
@@ -1,14 +1,32 @@
 use crate::utils::singly_linked_list::ListNode;
 
-pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
-    let mut dummy = ListNode { val: 0, next: head };
-    let mut cur = &mut dummy;
-    while let Some(node) = cur.next.as_mut() {
-        if node.val == val {
-            cur.next = node.next.take();
+pub fn remove_elements(mut head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+    let mut cur = head.as_mut()?;
+    while cur.val == val {
+        head = cur.next.take();
+        cur = head.as_mut()?;
+    }
+
+    while let Some(next) = cur.next.as_mut() {
+        if next.val == val {
+            cur.next = next.next.take();
         } else {
-            cur = cur.next.as_deref_mut().unwrap();
+            cur = cur.next.as_mut().unwrap();
         }
     }
-    dummy.next
+
+    head
 }
+
+// pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+//     let mut dummy = ListNode { val: 0, next: head };
+//     let mut cur = &mut dummy;
+//     while let Some(node) = cur.next.as_mut() {
+//         if node.val == val {
+//             cur.next = node.next.take();
+//         } else {
+//             cur = cur.next.as_deref_mut().unwrap();
+//         }
+//     }
+//     dummy.next
+// }
