f955d2a37ff8e5eb627e5c7a863c9158f5d01bb0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-01 14:36:18 +0800

    alternate

--- src/lc203/recursive.rs
+++ src/lc203/recursive.rs
@@ -1,15 +1,25 @@
 use crate::utils::singly_linked_list::ListNode;
 
 pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
-    match head {
-        None => None,
-        Some(mut node) => {
-            if node.val == val {
-                remove_elements(node.next.take(), val)
-            } else {
-                node.next = remove_elements(node.next.take(), val);
-                Some(node)
-            }
-        }
+    let mut node = head?;
+    node.next = remove_elements(node.next, val);
+    if node.val == val {
+        node.next
+    } else {
+        Some(node)
     }
 }
+
+// pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+//     match head {
+//         None => None,
+//         Some(mut node) => {
+//             if node.val == val {
+//                 remove_elements(node.next.take(), val)
+//             } else {
+//                 node.next = remove_elements(node.next.take(), val);
+//                 Some(node)
+//             }
+//         }
+//     }
+// }
