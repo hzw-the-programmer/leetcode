2d73d5ac8453fdd7d9f594100d00b74e6796739a
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-02 14:56:23 +0800

    alternative

--- src/lc876/mod.rs
+++ src/lc876/mod.rs
@@ -3,19 +3,46 @@
 use crate::utils::singly_linked_list::ListNode;
 
 pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut fast = head.as_ref()?;
+    let mut fast = head.as_ref();
     let mut slow = fast;
 
-    while let Some(node) = fast.next.as_ref() {
-        fast = node;
-        slow = slow.next.as_ref().unwrap();
-        if let Some(node) = fast.next.as_ref() {
-            fast = node;
-        }
+    while fast.is_some() && fast.unwrap().next.is_some() {
+        fast = fast.unwrap().next.as_ref().unwrap().next.as_ref();
+        slow = slow.unwrap().next.as_ref();
     }
 
-    Some(slow.clone())
+    slow.cloned()
 }
+
+// pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     let mut fast = head.as_ref();
+//     let mut slow = fast;
+
+//     while let Some(node) = fast {
+//         fast = node.next.as_ref();
+//         if let Some(node) = fast {
+//             fast = node.next.as_ref();
+//             slow = slow.unwrap().next.as_ref();
+//         }
+//     }
+
+//     slow.cloned()
+// }
+
+// pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     let mut fast = head.as_ref()?;
+//     let mut slow = fast;
+
+//     while let Some(node) = fast.next.as_ref() {
+//         fast = node;
+//         slow = slow.next.as_ref().unwrap();
+//         if let Some(node) = fast.next.as_ref() {
+//             fast = node;
+//         }
+//     }
+
+//     Some(slow.clone())
+// }
 
 // pub fn middle_node(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
 //     let mut fast: *mut ListNode = head.as_deref_mut()?;
