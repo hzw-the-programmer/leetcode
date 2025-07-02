b9893570db1816cae696df450c373f6a0090f76f
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-02 08:56:49 +0800

    optimize

--- src/lc206/recursive.rs
+++ src/lc206/recursive.rs
@@ -1,20 +1,35 @@
 use crate::utils::singly_linked_list::ListNode;
 
 pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    fn recursive(mut next: Box<ListNode>, cur: Box<ListNode>) -> Option<Box<ListNode>> {
-        match next.next.replace(cur) {
-            None => Some(next),
-            Some(node) => recursive(node, next),
+    fn recursive(head: Option<Box<ListNode>>, pre: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match head {
+            None => pre,
+            Some(mut node) => {
+                let next = node.next.take();
+                node.next = pre;
+                recursive(next, Some(node))
+            }
         }
     }
 
-    let mut cur = head?;
-    if let Some(next) = cur.next.take() {
-        recursive(next, cur)
-    } else {
-        Some(cur)
-    }
+    recursive(head, None)
 }
+
+// pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     fn recursive(cur: Box<ListNode>, mut next: Box<ListNode>) -> Option<Box<ListNode>> {
+//         match next.next.replace(cur) {
+//             None => Some(next),
+//             Some(node) => recursive(next, node),
+//         }
+//     }
+
+//     let mut cur = head?;
+//     if let Some(next) = cur.next.take() {
+//         recursive(cur, next)
+//     } else {
+//         Some(cur)
+//     }
+// }
 
 // pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
 //     fn recursive(mut next: Box<ListNode>, cur: Box<ListNode>) -> Option<Box<ListNode>> {
