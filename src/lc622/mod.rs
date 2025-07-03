18c01a20d284a678df87f66e426937131b890513
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-03 10:28:50 +0800

    queue use singly linked list

--- src/lc622/mod.rs
+++ src/lc622/mod.rs
@@ -1,10 +1,10 @@
 // 622. Design Circular Queue
 
-mod vec;
-pub use vec::MyCircularQueue;
+// mod vec;
+// pub use vec::MyCircularQueue;
 
-// mod linked_list;
-// pub use linked_list::MyCircularQueue;
+mod singly_linked_list;
+pub use singly_linked_list::MyCircularQueue;
 
 #[cfg(test)]
 mod tests;
