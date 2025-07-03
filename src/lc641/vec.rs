c54641d7eff2a413811502a7a9f34d7cfd8f9ed2
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-03 10:17:53 +0800

    refactor

---
+++ src/lc641/vec.rs
@@ -0,0 +1,80 @@
+pub struct MyCircularDeque {
+    elements: Vec<i32>,
+    rear: usize,
+    len: usize,
+}
+
+impl MyCircularDeque {
+    pub fn new(k: i32) -> Self {
+        Self {
+            elements: vec![0; k as usize],
+            rear: 0,
+            len: 0,
+        }
+    }
+
+    pub fn insert_front(&mut self, value: i32) -> bool {
+        if self.is_full() {
+            false
+        } else {
+            let idx = (self.rear + self.elements.len() - self.len - 1) % self.elements.len();
+            self.elements[idx] = value;
+            self.len += 1;
+            true
+        }
+    }
+
+    pub fn insert_last(&mut self, value: i32) -> bool {
+        if self.is_full() {
+            false
+        } else {
+            self.elements[self.rear] = value;
+            self.rear = (self.rear + 1) % self.elements.len();
+            self.len += 1;
+            true
+        }
+    }
+
+    pub fn delete_front(&mut self) -> bool {
+        if self.is_empty() {
+            false
+        } else {
+            self.len -= 1;
+            true
+        }
+    }
+
+    pub fn delete_last(&mut self) -> bool {
+        if self.is_empty() {
+            false
+        } else {
+            self.rear = (self.rear + self.elements.len() - 1) % self.elements.len();
+            self.len -= 1;
+            true
+        }
+    }
+
+    pub fn get_front(&self) -> i32 {
+        if self.is_empty() {
+            -1
+        } else {
+            self.elements[(self.rear + self.elements.len() - self.len) % self.elements.len()]
+        }
+    }
+
+    pub fn get_rear(&self) -> i32 {
+        if self.is_empty() {
+            -1
+        } else {
+            self.elements[(self.rear + self.elements.len() - 1) % self.elements.len()]
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.len == self.elements.len()
+    }
+}
