370810798a9e699c5291f30e25582d1c599b8028
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-03 21:13:47 +0800

    doubly linked list Box, *mut ListNode impl
    miri error

---
+++ src/lc641/doubly_linked_list_box.rs
@@ -0,0 +1,131 @@
+use core::ptr;
+
+struct ListNode {
+    val: i32,
+    next: Option<Box<ListNode>>,
+    pre: *mut ListNode,
+}
+
+impl ListNode {
+    fn new(val: i32) -> Self {
+        Self {
+            val,
+            next: None,
+            pre: ptr::null_mut(),
+        }
+    }
+}
+
+pub struct MyCircularDeque {
+    head: Option<Box<ListNode>>,
+    tail: *mut ListNode,
+    capacity: usize,
+    len: usize,
+}
+
+impl MyCircularDeque {
+    pub fn new(k: i32) -> Self {
+        Self {
+            head: None,
+            tail: ptr::null_mut(),
+            capacity: k as usize,
+            len: 0,
+        }
+    }
+
+    pub fn insert_front(&mut self, value: i32) -> bool {
+        if self.is_full() {
+            false
+        } else {
+            let mut node = Box::new(ListNode::new(value));
+            if self.head.is_none() {
+                self.head = Some(node);
+                self.tail = self.head.as_deref_mut().unwrap();
+            } else {
+                let mut head = self.head.take().unwrap();
+                head.pre = &mut *node;
+                node.next = Some(head);
+                self.head = Some(node);
+            }
+            self.len += 1;
+            true
+        }
+    }
+
+    pub fn insert_last(&mut self, value: i32) -> bool {
+        if self.is_full() {
+            false
+        } else {
+            let mut node = Box::new(ListNode::new(value));
+            if self.head.is_none() {
+                self.tail = &mut *node;
+                self.head = Some(node);
+            } else {
+                let tail = self.tail;
+                self.tail = &mut *node;
+                node.pre = tail;
+                unsafe { (*tail).next = Some(node) };
+            }
+            self.len += 1;
+            true
+        }
+    }
+
+    pub fn delete_front(&mut self) -> bool {
+        if self.is_empty() {
+            false
+        } else {
+            let mut head = self.head.take().unwrap();
+            if let Some(mut node) = head.next.take() {
+                node.pre = ptr::null_mut();
+                self.head = Some(node);
+            } else {
+                self.tail = ptr::null_mut();
+            }
+            self.len -= 1;
+            true
+        }
+    }
+
+    pub fn delete_last(&mut self) -> bool {
+        if self.is_empty() {
+            false
+        } else {
+            if unsafe { !(*self.tail).pre.is_null() } {
+                unsafe {
+                    (*(*self.tail).pre).next = None;
+                    self.tail = (*self.tail).pre;
+                }
+            } else {
+                self.head = None;
+                self.tail = ptr::null_mut();
+            }
+            self.len -= 1;
+            true
+        }
+    }
+
+    pub fn get_front(&self) -> i32 {
+        if self.is_empty() {
+            -1
+        } else {
+            self.head.as_ref().unwrap().val
+        }
+    }
+
+    pub fn get_rear(&self) -> i32 {
+        if self.is_empty() {
+            -1
+        } else {
+            unsafe { (*self.tail).val }
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.len == self.capacity
+    }
+}
--- src/lc641/mod.rs
+++ src/lc641/mod.rs
@@ -3,8 +3,11 @@
 // mod vec;
 // pub use vec::MyCircularDeque;
 
-mod doubly_linked_list_rc;
-pub use doubly_linked_list_rc::MyCircularDeque;
+// mod doubly_linked_list_rc;
+// pub use doubly_linked_list_rc::MyCircularDeque;
+
+mod doubly_linked_list_box;
+pub use doubly_linked_list_box::MyCircularDeque;
 
 #[cfg(test)]
 mod tests;
