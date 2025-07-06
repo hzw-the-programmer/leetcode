48d77b06e8dbfc38fda28129d2f23ad596f0f6a2
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-06 09:16:07 +0800

    singly dummy

--- src/lc707/mod.rs
+++ src/lc707/mod.rs
@@ -1,10 +1,13 @@
 // 707. Design Linked List
 
-mod singly;
-pub use singly::MyLinkedList;
+// mod singly;
+// pub use singly::MyLinkedList;
 
 // mod doubly;
 // pub use doubly::MyLinkedList;
+
+mod singly_dummy;
+pub use singly_dummy::MyLinkedList;
 
 #[cfg(test)]
 mod tests;
