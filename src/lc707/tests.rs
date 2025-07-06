32a38a5383a49f4e0099094bcba845a19680328c
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-06 16:22:13 +0800

    next and back test case

--- src/lc707/tests.rs
+++ src/lc707/tests.rs
@@ -179,3 +179,22 @@ fn mut_ref_into_iter() {
 
     assert_eq!(list.iter().copied().collect::<Vec<i32>>(), [2, 3, 4]);
 }
+
+#[test]
+fn next_and_back() {
+    let mut list = MyLinkedList::new();
+
+    list.add_at_tail(1);
+    list.add_at_tail(2);
+    list.add_at_tail(3);
+    list.add_at_tail(4);
+    list.add_at_tail(5);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next_back(), Some(&5));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next_back(), Some(&4));
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next_back(), None);
+}
