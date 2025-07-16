be01ab86287817b7af6161a0120323efd736c110
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-16 19:38:02 +0800

    Trie HashMap implementation

---
+++ src/lc208/hashmap.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+
+pub struct Trie {
+    children: HashMap<char, Trie>,
+    end: bool,
+}
+
+impl Trie {
+    pub fn new() -> Self {
+        Self {
+            children: HashMap::new(),
+            end: false,
+        }
+    }
+
+    pub fn insert(&mut self, word: String) {
+        let mut node = self;
+        for c in word.chars() {
+            node = node.children.entry(c).or_insert_with(|| Trie::new());
+        }
+        node.end = true;
+    }
+
+    pub fn search(&self, word: String) -> bool {
+        self.find(&word).map_or(false, |node| node.end)
+    }
+
+    pub fn starts_with(&self, prefix: String) -> bool {
+        self.find(&prefix).is_some()
+    }
+
+    fn find(&self, word: &str) -> Option<&Trie> {
+        let mut node = self;
+        for c in word.chars() {
+            node = node.children.get(&c)?;
+        }
+        Some(node)
+    }
+}
