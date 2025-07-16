658b0896080c920b005921f6a2d3ba73013ea5d0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-07-16 21:41:32 +0800

    refactr

--- src/lc155/mod.rs
+++ src/lc155/mod.rs
@@ -1,43 +1,7 @@
 // 155. Min Stack
 
-struct MinStack {
-    data: Vec<i32>,
-    min: Vec<i32>,
-}
-
-impl MinStack {
-    fn new() -> Self {
-        Self {
-            data: vec![],
-            min: vec![],
-        }
-    }
-
-    fn push(&mut self, val: i32) {
-        self.data.push(val);
-        match self.min.last() {
-            None => self.min.push(val),
-            Some(&v) if val <= v => self.min.push(val),
-            _ => {}
-        }
-    }
-
-    fn pop(&mut self) {
-        self.data.pop().map(|v| {
-            if v == *self.min.last().unwrap() {
-                self.min.pop();
-            }
-        });
-    }
-
-    fn top(&self) -> i32 {
-        *self.data.last().unwrap()
-    }
-
-    fn get_min(&self) -> i32 {
-        *self.min.last().unwrap()
-    }
-}
+mod data_min;
+pub use data_min::MinStack;
 
 #[cfg(test)]
 mod tests;
